b22e0f41039c8e192b8b0202f4f83835f1096a66
Alwin Esch <alwin.esch@web.de>
2018-10-06 21:43:14 +0200

    Fix OpenGL Rendering

---
+++ screensaver.asteroids/resources/shaders/GL/3.3/frag.glsl
@@ -0,0 +1,9 @@
+#version 330 core
+
+in vec4 m_col;
+out vec4 FragColor;
+
+void main()
+{
+   FragColor = m_col;
+}
---
+++ screensaver.asteroids/resources/shaders/GL/3.3/vert.glsl
@@ -0,0 +1,11 @@
+#version 330 core
+
+layout (location = 0) in vec3 m_attrpos;
+in vec4 m_attrcol;
+out vec4 m_col;
+
+void main()
+{
+   gl_Position = vec4(m_attrpos.x, m_attrpos.y, m_attrpos.z, 1.0);
+   m_col = m_attrcol;
+}
